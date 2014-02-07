225ee8d5de8fd54595fae0a9d14b050f2f539bfd
Sanghee Kim <sangheestyle@gmail.com>
2014-02-07 14:55:52 -0700

    refactor method names following ruby coding convention
    'Methods should be verbs.' for following ruby coding convention.
    http://rails.nuvvo.com/lesson/5017-ruby-coding-convention

--- apk_ui_contents_set.rb
+++ apk_ui_contents_set.rb
@@ -15,20 +15,20 @@ class ApkUIContentsSet
   attr_reader :apk_ui_contents_set
 
   def initialize(path)
-    @apk_ui_contents_set = self.from(path)
+    @apk_ui_contents_set = self.read(path)
   end
 
-  def from(path)
+  def read(path)
     apk_ui_contents = Array.new
     if File.directory?(path)
-      apk_ui_contents << self.from_directory(path)
+      apk_ui_contents << self.read_directory(path)
     else
-      apk_ui_contents << self.from_file(path)
+      apk_ui_contents << self.read_file(path)
     end
     apk_ui_contents.flatten
   end
 
-  def from_file(file_path)
+  def read_file(file_path)
     file_name = Pathname.new(file_path).basename.to_s
     apk_name = file_name[0..-7]
     version = file_name[-5]
@@ -37,10 +37,10 @@ class ApkUIContentsSet
     ApkUIContents.new(apk_name, version, contents)
   end
 
-  def from_directory(path)
+  def read_directory(path)
     apk_ui_contents = Array.new
     Dir[path+"/**/*-[a-z].txt"].each do |file_path|
-      apk_ui_contents << self.from_file(file_path)
+      apk_ui_contents << self.read_file(file_path)
     end
     apk_ui_contents.flatten
   end
