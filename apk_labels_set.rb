fa0966c778283246f1d69abf111465de343a0ccb
Sanghee Kim <sangheestyle@gmail.com>
2014-02-07 16:00:06 -0700

    from labels.txt, making a set of labels (only a file not directory)
    FIXME: need to implement read_dictionary.
    At this time, I want to read only a file to make a set of labels
    for rapid prototyping.

--- apk_labels_set.rb
+++ apk_labels_set.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/ruby
+require "csv"
 
 class ApkLabels
   attr_reader :apk_name, :id, :labels
@@ -9,3 +10,42 @@ class ApkLabels
     @labels = labels
   end
 end
+
+class ApkLabelsSet
+  attr_reader :apk_labels_set
+
+  def initialize(path)
+    @apk_labels_set = self.read(path)
+  end
+
+  def read(path)
+    apk_labels_set = Array.new
+    if File.directory?(path)
+      apk_labels_set << self.read_directory(path)
+    else
+      apk_labels_set << self.read_file(path)
+    end
+    apk_labels_set.flatten
+  end
+
+  def read_file(path)
+    apk_labels_set = Array.new
+    CSV.foreach(path) do |row|
+      row = row.collect{|x| x.strip}
+      apk_labels = ApkLabels.new(row[0], row[1], row[2..-1])
+      apk_labels_set << apk_labels
+    end
+    return apk_labels_set
+  end
+
+  def read_directory(path)
+    raise NotImplementedError
+  end
+end
+
+=begin : for simple testing
+al_set = ApkLabelsSet.new(ARGV[0])
+al_set.apk_labels_set.each do |labels|
+  puts labels.apk_name, labels.id, labels.labels
+end
+=end
