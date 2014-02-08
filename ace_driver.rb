41dd3aa5fa93e28172b97925b96eb21245071d05
Sanghee Kim <sangheestyle@gmail.com>
2014-02-07 22:43:50 -0700

    Test export evaluations into csv file
    usage:
    $ ruby ace_driver.rb demo/data/labels/labels.txt demo apk eval_apk.txt
    $ ruby ace_driver.rb demo/data/labels/labels.txt demo case eval_case.txt

--- ace_driver.rb
+++ ace_driver.rb
@@ -1,8 +1,7 @@
 #!/usr/bin/ruby
 require_relative 'apk_contents_evaluator'
 
-path_labels, path_uis = ARGV[0], ARGV[1]
+path_labels, path_uis, eval_type, csv_path = ARGV
 ace = ApkContentsEvaluator.new(path_labels, path_uis)
 ace.calc_eval()
-p ace.eval_per_case
-p ace.eval_per_apk
+ace.to_csv(eval_type, csv_path)
